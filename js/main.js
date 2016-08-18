c88615a25cd088b5e4a92080314d4c3ecbd340bf
Ja Shia <ja.shia@revelsystems.com>
2016-08-18 14:39:12 -0700

    moves the data and more anchors

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		anchors: ["revelin", "sales", "fees", "promo", "integrate", "more", "data" ],
+		anchors: ["revelin", "sales", "fees", "promo", "integrate", "data", "more" ],
 		menu: '#menu',
 		css3: true,
         scrollingSpeed: 1000,
         navigation: true,
         navigationPosition: "right",
-        navigationTooltips: [ "revelin", "sales", "fees", "promo", "integrate", "more", "data" ],
+        navigationTooltips: [ "revelin", "sales", "fees", "promo", "integrate", "data", "more" ],
         afterLoad: function(anchorLink) {
         	var sectionClass = anchorLink;
         	if ( sectionClass !== "revelin") {
